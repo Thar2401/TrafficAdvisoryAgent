01a1513f54e88648dab5d43976ce0885a3aced1a
Tharun <142334894+Thar2401@users.noreply.github.com>
2026-01-27 21:30:46 +0530

    Delete TrafficAdvisoryAgent directory

--- TrafficAdvisoryAgent/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# Models package for Traffic Advisory Agent
-
-from .traffic_predictor import TrafficPredictor
-from .route_optimizer import RouteOptimizer
-from .sustainability_calculator import SustainabilityCalculator
-
-__all__ = ['TrafficPredictor', 'RouteOptimizer', 'SustainabilityCalculator']
--- TrafficAdvisoryAgent/utils/__init__.py
+++
@@ -1,7 +0,0 @@
-# Utils package for Traffic Advisory Agent
-
-from .config import Config
-from .validators import InputValidator
-from .data_generator import TrafficDataGenerator
-
-__all__ = ['Config', 'InputValidator', 'TrafficDataGenerator']
